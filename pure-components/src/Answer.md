8def07e9f253204f227a5213f7885761ca1cd36c
aayushwrld <aayush.arora@kalvium.community>
2023-12-27 12:09:05 +0530

    final

---
+++ pure-components/src/Answer.md
@@ -0,0 +1,3 @@
+**Which out of the two components (which are performing the same functionality) is performing better and how?**
+
+Ans - Pure component is performing better as compared to the simple component as, nothing is rendering when toggle is off. Increasing performance impact. There is optimization in the rendering.
--- pure-components/src/Components/SimpleCounterComponent.jsx
+++ pure-components/src/Components/SimpleCounterComponent.jsx
@@ -1,42 +1,44 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class SimpleCounterComponent extends Component {
-    constructor(){
-        super();
-        this.state = {
-            count: 0,
-            toggle: true
-        }
-    }
-  render() {
-    console.warn("This is a simple component")
-    return (
-        <>
-        <hr />
-      <h2>Simple Counter Component</h2>
-      <h2>Count {this.state.count}</h2>
-      <br />
-      <button onClick={()=>{
-        this.setState({
-            toggle: !this.state.toggle
-        })
-      }}
-      style={{
-        backgroundColor: `${this.state.toggle ? "green" : "red"}`
-      }}
-      >Toggle</button>
-      <br /><br />
-      <button onClick={()=>{
-        if (this.state.toggle){
-            this.setState({
-                count: this.state.count + 1
-            })
-        }
-      }}
-      >Counter</button>
-      <hr />
-        </>
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+      toggle: true,
+    };
+  }
 
-    )
+  render() {
+    console.warn("This is a simple Component");
+    return (
+      <div>
+        <h2>Simple Component</h2>
+        <h3>{this.state.count}</h3>
+        <button
+          style={{
+            backgroundColor: `${this.state.toggle ? "green" : "red"}`,
+            color: "white",
+          }}
+          onClick={() => {
+            this.setState({ toggle: !this.state.toggle });
+          }}
+        >
+          Toggle
+        </button>
+        <button
+          onClick={() => {
+            if (this.state.toggle) {
+              this.setState({ count: this.state.count + 1 });
+            } 
+            else {
+              this.setState({ count: this.state.count });
+            }
+          }}
+        >
+          Counter
+        </button>
+      </div>
+    );
   }
 }
